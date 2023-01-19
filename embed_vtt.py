3882c0cf46bbe1b36569fe12e4c47183a0038d24
chadlohrli <chad.david.lohrli@gmail.com>
2023-01-19 15:32:12 -0800

    turn script into class & add argparse

--- embed_vtt.py
+++ embed_vtt.py
@@ -1,86 +1,122 @@
+from openai.embeddings_utils import get_embedding
+from dotenv import load_dotenv
+from ast import literal_eval
+from tqdm import tqdm
+
 import pinecone
 import webvtt
 import openai
-from openai.embeddings_utils import get_embedding
 import pandas as pd
 import tiktoken
 import random
-from ast import literal_eval
 import itertools
-
 import os
-from dotenv import load_dotenv
+import argparse
+
 load_dotenv()
 
-# read key from .env file
-openai.api_key = os.getenv('OPENAI_KEY')
+# create exception class for embedding class
+class EmbeddingException(Exception):
+    pass
 
-def read_data():
-    captions = webvtt.read('gpt_tutorial.vtt')
-    text = [caption.text for caption in captions]
-    df = pd.DataFrame(text, columns=['captions'])
-    return df
+class Embeddings:
+    def __init__(self, df):
+        # embedding dataframe
+        self._df = df
+        # openai parameters
+        self._embedding_encoding = "cl100k_base"
+        self._embedding_model = "text-embedding-ada-002"
+        # load keys
+        try:
+            self._openai_key = os.getenv('OPENAI_KEY')
+            self._pinecone_key = os.getenv('PINECONE_KEY')
+            openai.api_key = self._openai_key
+        except:
+            raise EmbeddingException("No API keys found")
+        # init pinecone
+        pinecone.init(api_key=self._pinecone_key, environment="us-west1-gcp")
+        # use first index in list
+        self._pinecone_index = pinecone.Index(pinecone.list_indexes()[0])
+            
+    @classmethod
+    def read_vtt_file(cls, file_path):
+        captions = webvtt.read(file_path)
+        # TODO: so inefficient, fix this
+        text = [caption.text for caption in captions]
+        df = pd.DataFrame(text, columns=['captions'])
+        return df
 
-def get_encodings(df):
-    embedding_encoding = "cl100k_base"  # this the encoding for text-embedding-ada-002
+    @classmethod
+    def read_csv_embedding_file(cls, file_path):
+        df = pd.read_csv(file_path)
+        # literal_eval needed to parse string to list
+        df["embedding"] = df["embedding"].apply(lambda x: literal_eval(x))
+        return df
 
-    encoding = tiktoken.get_encoding(embedding_encoding)
-    df["n_tokens"] = df["captions"].apply(lambda x: len(encoding.encode(x)))
+    def get_encodings(self):
+        encoding = tiktoken.get_encoding(self._embedding_encoding)
+        self._df["n_tokens"] = self._df["captions"].apply(lambda x: len(encoding.encode(x)))
 
-    return df
+    def get_embeddings(self):
+        tqdm.pandas(desc="generating embeddings")
+        self._df["embedding"] = self._df["captions"].progress_apply(
+            lambda x: get_embedding(x, engine=self._embedding_model))
+        
+    def save_embeddings_to_csv(self, file_name):
+        self._df.to_csv(file_name)
 
-def get_embeddings(df):
-    embedding_model = "text-embedding-ada-002"
-    df["embedding"] = df["captions"].apply(
-        lambda x: get_embedding(x, engine=embedding_model))
-    
-    df.to_csv("gpt_tutorial_embeds.csv")
+    def upload_embeddings(self):
+        # A helper function to break an iterable into chunks of size batch_size
+        def chunks(iterable, batch_size=100):
+            it = iter(iterable)
+            chunk = tuple(itertools.islice(it, batch_size))
+            while chunk:
+                yield chunk
+                chunk = tuple(itertools.islice(it, batch_size))
 
-def generate_embeddings():
-    df = read_data()
-    encodings = get_encodings(df)
-    embeddings = get_embeddings(df)
+        df = self._df
 
-######
+        example_data_generator = map(lambda i: (
+            f'id-{i}', df["embedding"][i], {"captions": df["captions"][i]}), range(len(df["embedding"])))
 
-def query_embeddings(query, index):
-    xq = openai.Embedding.create(input=query, engine="text-embedding-ada-002")['data'][0]['embedding']
-    # query, returning the top 5 most similar results
-    res = index.query([xq], top_k=5, include_metadata=True)
-    for match in res['matches']:
-        print(f"{match['score']:.2f}: {match['metadata']['captions']}")
+        for ids_vectors_chunk in chunks(example_data_generator, batch_size=100):
+            print("uploading chunk")
+            self._pinecone_index.upsert(vectors=ids_vectors_chunk)
 
-def chunks(iterable, batch_size=100):
-    """A helper function to break an iterable into chunks of size batch_size."""
-    it = iter(iterable)
-    chunk = tuple(itertools.islice(it, batch_size))
-    while chunk:
-        yield chunk
-        chunk = tuple(itertools.islice(it, batch_size))
-
-def upload_embeddings(index):
-    df = pd.read_csv("gpt_tutorial_embeds.csv")
-
-    df["embedding"] = df["embedding"].apply(lambda x: literal_eval(x))
-    example_data_generator = map(lambda i: (
-        f'id-{i}', df["embedding"][i], {"captions": df["captions"][i]}), range(len(df["embedding"])))
-
-    for ids_vectors_chunk in chunks(example_data_generator, batch_size=100):
-        print("uploading chunk")
-        index.upsert(vectors=ids_vectors_chunk)
-
-def init_pinecone():
-    pinecone_key = os.getenv('PINECONE_KEY')
-    pinecone.init(api_key=pinecone_key,
-                environment="us-west1-gcp")
-    index = pinecone.Index(pinecone.list_indexes()[0])
-    return index
+    def query_embeddings(self, query, top_k=5):
+        embed = openai.Embedding.create(input=query, engine="text-embedding-ada-002")['data'][0]['embedding']
+        # query, returning the top 5 most similar results
+        res = self._pinecone_index.query([embed], top_k=top_k, include_metadata=True)
+        for match in res['matches']:
+            print(f"{match['score']:.2f}: {match['metadata']['captions']}")
 
 def main():
-    index = init_pinecone()
-    #upload_embeddings(index)
-    query_embeddings("the usefulness of tril tensors", index)
+    parser = argparse.ArgumentParser('Embed-Karpathy')
+    subparsers = parser.add_subparsers(dest="command", required=True)
+
+    generate = subparsers.add_parser('generate', help='generate embeddings')
+    generate.add_argument('--vtt-file', type=str, help='vtt file path')
+
+    generate = subparsers.add_parser('upload', help='upload embeddings to pinecone')
+    generate.add_argument('--csv-embedding-file', type=str, help='csv embeddings file path')
+
+    query = subparsers.add_parser('query', help='query pinecone')
+    query.add_argument('--text', type=str, help='text to query')
+
+    args = parser.parse_args()
+    if args.command == "generate":
+        df = Embeddings.read_vtt_file(args.vtt_file)
+        embed = Embeddings(df)
+        embed.get_encodings()
+        embed.get_embeddings()
+        embed.save_embeddings_to_csv("embeddings.csv")
+    elif args.command == "upload":
+        df = Embeddings.read_csv_embedding_file(args.csv_embedding_file)
+        embed = Embeddings(df)
+        embed.upload_embeddings()
+    elif args.command == "query":
+        embed = Embeddings(None)
+        embed.query_embeddings(args.text)
 
 if __name__ == '__main__':
-    df = None
     main()
